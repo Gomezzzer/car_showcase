9b71639f20385b5d720c34499dac625f29f436d1
gomezzzer <gomezgeorge132@gmail.com>
2024-09-17 18:46:41 -0500

    car images

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+
+
 import { CustomFilter, Hero, SearchBar } from "@/components";
 import CarCard from "@/components/CarCard";
 import ShowMore from "@/components/ShowMore";
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.imagin.studio']
+        domains: ['cdn.imagin.studio', 'api.carscan.com']
     }
 };
 
 export default nextConfig;
+
